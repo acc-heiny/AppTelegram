6f58517b54c471a0b10b14803e41b110bd2a7a09
Hellen Heiny Feitoza de Souza Santi <hellen.heiny.f.santi@accenture.com>
2023-08-21 23:15:20 -0300

    Initial Commit

---
+++ AppTelegram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTelegram
+//
+//  Created by hellen.heiny.f.santi on 21/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
